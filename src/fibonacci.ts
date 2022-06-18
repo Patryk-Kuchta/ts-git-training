a17d9f7cf1144563b9c474f4624ed235f15e4a2a
Cameron McCormack <cameron.mccormack@softwire.com>
2022-06-18 09:41:57 +0100

    allow an optional negative recursive approach

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,9 +1,11 @@
-export const computeFibonacciNumber = (position: number | null): number => {
+export const computeFibonacciNumber = (position: number, isRecursive: boolean = false): number => {
+    if (isRecursive) return recursiveFibonacci(position);
+
     let notNullPosition = position;
     if (notNullPosition === null) {
-        notNullPosition = 1;   
+        notNullPosition = 1;
     }
-  
+
     if (notNullPosition === 0) {
         return 0;
     }
@@ -14,6 +16,10 @@ export const computeFibonacciNumber = (position: number | null): number => {
     let i = 1;
     let j = 1;
 
+    if (position <= 2) {
+        return 1;
+    }
+
     let currentPosition = 2;
     while (currentPosition < notNullPosition) {
         const temp = i;
@@ -23,6 +29,17 @@ export const computeFibonacciNumber = (position: number | null): number => {
     }
     return j;
 };
+
+const recursiveFibonacci = (initialPosition: number, left: number = 0, right: number = 1, position?: number): number => {
+    const currentPosition = position ?? initialPosition;
+    if (initialPosition === 0) return 0;
+    if (currentPosition === 0) return left;
+    if (initialPosition > 0) {
+        return recursiveFibonacci(initialPosition, right, left + right, currentPosition - 1);
+    } else {
+        return recursiveFibonacci(initialPosition, right - left, left, currentPosition + 1);
+    }
+}
 
 export const computeFibonacciArray = (start: number, endInclusive: number): number[] => {
     const inputArray = [...Array(endInclusive - start + 1).keys()].map(i => i + start);
